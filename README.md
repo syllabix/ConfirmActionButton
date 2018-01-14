7a58333ecb5859e132d66ecabb387b7784afbdb2
Tom Stoepker <tom.stoepker@cqlcorp.com>
2018-01-14 16:52:49 -0500

    fixing default animatable, updating readme, and including working sample

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # ConfirmActionButton
-A data bindable floating action button for android that supports a tap twice to confirm workflow.
+
+| Confirm Action Button | About |
+|:-------------------:|:------|
+| ![confirm-button-demo](https://user-images.githubusercontent.com/5419021/34921044-b2449924-f94a-11e7-82b2-64e314e78589.gif) | An early stage implementatio of a data bindable floating action button for android that supports a tap twice to confirm workflow.
+
+---
+
 
 ## Install
 
@@ -8,8 +14,7 @@ Grade:
 ```implementation 'com.crushonly.confirmactionbutton:confirmactionbutton:0.0.1'```
 
 ### Notes and Todos
-This project is in very early stages of development. 
+This project is in very early stages of development, and while it does provide a functional component - levels of customization are limited. 
 
-1. Sample App / .gifs of working widget
-2. Improve Test Coverage
-3. Expand widget styleable attributes
+1. Improve Test Coverage
+2. Expand widget styleable attributes
--- sample/src/main/java/com/crushonly/sample/MainActivity.kt
+++ sample/src/main/java/com/crushonly/sample/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.crushonly.sample
 
+import android.databinding.DataBindingUtil
+import android.databinding.ObservableField
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.crushonly.confirmactionbutton.ConfirmActionButton
+import com.crushonly.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val confirmActionMode = ObservableField<ConfirmActionButton.Mode>()
+    val confirmButtonMessage = ObservableField<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding?.setVariable(BR.activity, this)
+        confirmActionMode.set(ConfirmActionButton.Mode.READY)
+    }
+
+    fun handleClick() {
+        confirmButtonMessage.set("Confirm Action?")
+        confirmActionMode.set(ConfirmActionButton.Mode.CONFIRMING)
+    }
+
+    fun handleConfirmClick() {
+        confirmButtonMessage.set("")
+        confirmActionMode.set(ConfirmActionButton.Mode.READY)
     }
 }
